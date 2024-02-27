5a7667f0bfd1efcbef103cd4c141bc5683d623fe
kangyesong <kys0017@gmail.com>
2024-02-27 21:14:58 +0900

    👽️ Apollo Setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
+import { ApolloProvider } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import { client } from "./apollo";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
