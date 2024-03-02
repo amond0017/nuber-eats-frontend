c2df41160f027ef7a391872a594cd23919027529
kangyesong <kys0017@gmail.com>
2024-03-03 01:37:39 +0900

    🛂 Using the Token - ApolloClient 에 헤더 설정

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,11 +1,32 @@
-import React from "react";
-import { isLoggedInVar } from "../apollo";
+import { gql, useQuery } from "@apollo/client";
+import { meQuery } from "../__generated__/meQuery";
 
-const LoggedInRouter = () => (
-  <div>
-    <h1>Logged In</h1>
-    <button onClick={() => isLoggedInVar(false)}>Log Out</button>
-  </div>
-);
+const ME_QUERY = gql`
+  query meQuery {
+    me {
+      id
+      email
+      role
+      verified
+    }
+  }
+`;
+
+const LoggedInRouter = () => {
+  const { data, loading, error } = useQuery<meQuery>(ME_QUERY);
+  // console.log(data);
+  if (!data || loading || error) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <span className="font-medium text-xl tracking-wide">Loading...</span>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>{data.me.email}</h1>
+    </div>
+  );
+};
 
 export default LoggedInRouter;
