dac9699ea325e8fdebb361b5973c64b65f601fb1
kangyesong <kys0017@gmail.com>
2024-03-05 01:47:45 +0900

    🎨 Edit Profile part Two - mutation gql, onSubmit 작성

--- src/pages/user/edit-profile.tsx
+++ src/pages/user/edit-profile.tsx
@@ -1,16 +1,84 @@
+import { gql, useMutation } from "@apollo/client";
+import { editProfile, editProfileVariables } from "@generated/editProfile";
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Button } from "src/components/button";
 import { useMe } from "src/hooks/useMe";
 
+const EDIT_PROFILE_MUTATION = gql`
+  mutation editProfile($input: EditProfileInput!) {
+    editProfile(input: $input) {
+      ok
+      error
+    }
+  }
+`;
+
+interface IFormProps {
+  email?: string;
+  password?: string;
+}
+
 export const EditProfile = () => {
   const { data: userData } = useMe();
+  const onCompleted = (data: editProfile) => {
+    const {
+      editProfile: { ok },
+    } = data;
+    if (ok) {
+      // update the cache
+    }
+  };
+  const [editProfile, { loading }] = useMutation<
+    editProfile,
+    editProfileVariables
+  >(EDIT_PROFILE_MUTATION, {
+    onCompleted,
+  });
+  const { register, handleSubmit, getValues, formState } = useForm<IFormProps>({
+    defaultValues: {
+      email: userData?.me.email,
+    },
+  });
+  const onSubmit = () => {
+    const { email, password } = getValues();
+    editProfile({
+      variables: {
+        input: {
+          email,
+          ...(password !== "" && { password }),
+        },
+      },
+    });
+  };
+
   return (
     <div className="mt-52 flex flex-col items-center justify-center">
       <h4 className="font-semibold text-2xl mb-3">Edit Profile</h4>
-      <form className="grid max-w-screen-sm gap-3 mt-5 w-full mb-5">
-        <input className="input" type="email" placeholder="Email" />
-        <input className="input" type="password" placeholder="Password" />
-        <Button loading={false} canClick actionText="Save Profile" />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid max-w-screen-sm gap-3 mt-5 w-full mb-5"
+      >
+        <input
+          {...register("email", {
+            pattern:
+              /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          })}
+          className="input"
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          {...register("password")}
+          className="input"
+          type="password"
+          placeholder="Password"
+        />
+        <Button
+          loading={loading}
+          canClick={formState.isValid}
+          actionText="Save Profile"
+        />
       </form>
     </div>
   );
