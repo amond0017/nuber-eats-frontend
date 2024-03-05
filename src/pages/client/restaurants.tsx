1aa2bcf65d3ef36565880b2b650991b855be597c
kangyesong <kys0017@gmail.com>
2024-03-05 20:10:46 +0900

    📝 Reataurants Pagination - 페이지네이션 구현

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -3,7 +3,8 @@ import {
   restarantsPageQuery,
   restarantsPageQueryVariables,
 } from "@generated/restarantsPageQuery";
-import React from "react";
+import React, { useState } from "react";
+import { Restaurant } from "src/components/restaurant";
 
 const RESTAURANTS_QUERY = gql`
   query restarantsPageQuery($input: RestaurantsInput!) {
@@ -38,16 +39,20 @@ const RESTAURANTS_QUERY = gql`
 `;
 
 export const Restaurants = () => {
+  const [page, setPage] = useState(1);
   const { data, loading } = useQuery<
     restarantsPageQuery,
     restarantsPageQueryVariables
   >(RESTAURANTS_QUERY, {
     variables: {
       input: {
-        page: 1,
+        page,
       },
     },
   });
+
+  const onNextPageClick = () => setPage((current) => current + 1);
+  const onPrevPageClick = () => setPage((current) => current - 1);
 
   return (
     <div>
@@ -59,7 +64,7 @@ export const Restaurants = () => {
         />
       </form>
       {!loading && (
-        <div className="max-w-screen-2xl mx-auto mt-8">
+        <div className="max-w-screen-2xl pb-20 mx-auto mt-8">
           <div className="flex justify-around max-w-sm mx-auto">
             {data?.allCategories.categories?.map((category) => (
               <div
@@ -77,17 +82,41 @@ export const Restaurants = () => {
             ))}
           </div>
 
-          <div className="grid mt-5 grid-cols-3 gap-x-5 gap-y-10">
+          <div className="grid mt-16 grid-cols-3 gap-x-5 gap-y-10">
             {data?.restaurants.results?.map((restaurant) => (
-              <div>
-                <div
-                  style={{ backgroundImage: `url(${restaurant.coverImg})` }}
-                  className="bg-red-500 bg-cover bg-center mb-3 py-28"
-                ></div>
-                <h3 className="text-xl font-medium">{restaurant.name}</h3>
-                <span>{restaurant.category?.name}</span>
-              </div>
+              <Restaurant
+                key={restaurant.id + ""}
+                id={restaurant.id + ""}
+                coverImg={restaurant.coverImg}
+                name={restaurant.name}
+                categoryName={restaurant.category?.name}
+              />
             ))}
+          </div>
+          <div className="grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10">
+            {page > 1 ? (
+              <button
+                onClick={onPrevPageClick}
+                className="focus:outline-none text-2xl font-medium"
+              >
+                &larr;
+              </button>
+            ) : (
+              <div></div>
+            )}
+            <span>
+              Page {page} of {data?.restaurants.totalPages}
+            </span>
+            {page !== data?.restaurants.totalPages ? (
+              <button
+                onClick={onNextPageClick}
+                className="focus:outline-none text-2xl font-medium"
+              >
+                &rarr;
+              </button>
+            ) : (
+              <div></div>
+            )}
           </div>
         </div>
       )}
