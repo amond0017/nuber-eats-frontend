89bb9b187287173612b4e4b485c75fb7ed9e2fab
kangyesong <kys0017@gmail.com>
2024-03-07 19:30:44 +0900

    💄 Code Challenge - category, search 페이지

--- src/pages/client/category.tsx
+++ src/pages/client/category.tsx
@@ -1,7 +1,13 @@
 import { gql, useQuery } from "@apollo/client";
 import { category, categoryVariables } from "@generated/category";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
+import { Link, useParams } from "react-router-dom";
+import { Restaurant } from "src/components/restaurant";
+import { SearchForm } from "src/components/search-form";
 import { CATEGORY_FRAGMENT, RESTAURANT_FRAGMENT } from "src/fragments";
+import { useRestaurants } from "src/hooks/useRestaurants";
+import { toPascalCase } from "src/lib/common";
 
 const CATEGORY_QUERY = gql`
   query category($input: CategoryInput!) {
@@ -27,20 +33,93 @@ interface ICategoryParams {
 }
 
 export const Category = () => {
+  const [page, setPage] = useState(1);
+
+  const restaurantsQuery = useRestaurants({ page });
+
   const params = useParams<ICategoryParams>();
-  const { loading, data } = useQuery<category, categoryVariables>(
-    CATEGORY_QUERY,
-    {
-      variables: {
-        input: {
-          page: 1,
-          slug: params.slug,
-        },
+  const categoryQuery = useQuery<category, categoryVariables>(CATEGORY_QUERY, {
+    variables: {
+      input: {
+        page: 1,
+        slug: params.slug,
       },
-    }
+    },
+  });
+
+  useEffect(() => {
+    setPage(1);
+  }, [params.slug]);
+
+  const onNextPageClick = () => setPage((current) => current + 1);
+  const onPrevPageClick = () => setPage((current) => current - 1);
+
+  return (
+    <div>
+      <Helmet>
+        <title>{toPascalCase(params.slug)} | Nuber Eats</title>
+      </Helmet>
+
+      <SearchForm />
+
+      {!(categoryQuery.loading || restaurantsQuery.loading) && (
+        <div className="max-w-screen-2xl pb-20 mx-auto mt-8">
+          <div className="flex justify-around max-w-sm mx-auto">
+            {restaurantsQuery.data?.allCategories.categories?.map(
+              (category) => (
+                <Link to={`/category/${category.slug}`} key={category.slug}>
+                  <div className="flex flex-col group items-center cursor-pointer">
+                    <div
+                      className="w-16 h-16 bg-cover group-hover:bg-gray-100 rounded-full"
+                      style={{ backgroundImage: `url(${category.coverImg})` }}
+                    ></div>
+                    <span className="mt-1 text-sm font-medium">
+                      {category.name}
+                    </span>
+                  </div>
+                </Link>
+              )
+            )}
+          </div>
+
+          <div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
+            {categoryQuery.data?.category.restaurants?.map((restaurant) => (
+              <Restaurant
+                key={restaurant.id + ""}
+                id={restaurant.id + ""}
+                coverImg={restaurant.coverImg}
+                name={restaurant.name}
+                categoryName={restaurant.category?.name}
+              />
+            ))}
+          </div>
+          <div className="grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10">
+            {page > 1 ? (
+              <button
+                onClick={onPrevPageClick}
+                className="focus:outline-none text-2xl font-medium"
+              >
+                &larr;
+              </button>
+            ) : (
+              <div></div>
+            )}
+            <span>
+              Page {page} of {categoryQuery.data?.category?.totalPages}
+            </span>
+            {page !== categoryQuery.data?.category?.totalPages ? (
+              <button
+                onClick={onNextPageClick}
+                className="focus:outline-none text-2xl font-medium"
+              >
+                &rarr;
+              </button>
+            ) : (
+              <div></div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
   );
-
-  console.log(data);
-
-  return <div>Category</div>;
 };
--- src/pages/client/search.tsx
+++ src/pages/client/search.tsx
@@ -3,10 +3,13 @@ import {
   searchRestaurant,
   searchRestaurantVariables,
 } from "@generated/searchRestaurant";
-import React, { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Helmet } from "react-helmet-async";
-import { useHistory, useLocation } from "react-router-dom";
+import { Link, useHistory, useLocation } from "react-router-dom";
+import { Restaurant } from "src/components/restaurant";
+import { SearchForm } from "src/components/search-form";
 import { RESTAURANT_FRAGMENT } from "src/fragments";
+import { useRestaurants } from "src/hooks/useRestaurants";
 
 const SEARCH_RESTAURANT = gql`
   query searchRestaurant($input: SearchRestaurantInput!) {
@@ -26,16 +29,22 @@ const SEARCH_RESTAURANT = gql`
 export const Search = () => {
   const location = useLocation();
   const history = useHistory();
+
+  const [page, setPage] = useState(1);
+
+  const restaurantsQuery = useRestaurants({ page });
   const [callQuery, { loading, data, called }] = useLazyQuery<
     searchRestaurant,
     searchRestaurantVariables
   >(SEARCH_RESTAURANT);
 
+  const query = useMemo(() => location.search.split("?term=")[1], [location]);
+
   useEffect(() => {
-    const [, query] = location.search.split("?term=");
     if (!query) {
       return history.replace("/");
     }
+    setPage(1);
 
     callQuery({
       variables: {
@@ -45,14 +54,77 @@ export const Search = () => {
         },
       },
     });
-  }, [history, location]);
+  }, [history, query]);
+
+  const onNextPageClick = () => setPage((current) => current + 1);
+  const onPrevPageClick = () => setPage((current) => current - 1);
 
   return (
     <div>
       <Helmet>
-        <title>Search | Nuber Eats</title>
+        <title>Search - {query} | Nuber Eats</title>
       </Helmet>
-      Search
+
+      <SearchForm />
+
+      {!(loading || restaurantsQuery.loading) && (
+        <div className="max-w-screen-2xl pb-20 mx-auto mt-8">
+          <div className="flex justify-around max-w-sm mx-auto">
+            {restaurantsQuery.data?.allCategories.categories?.map(
+              (category) => (
+                <Link to={`/category/${category.slug}`} key={category.slug}>
+                  <div className="flex flex-col group items-center cursor-pointer">
+                    <div
+                      className="w-16 h-16 bg-cover group-hover:bg-gray-100 rounded-full"
+                      style={{ backgroundImage: `url(${category.coverImg})` }}
+                    ></div>
+                    <span className="mt-1 text-sm font-medium">
+                      {category.name}
+                    </span>
+                  </div>
+                </Link>
+              )
+            )}
+          </div>
+
+          <div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
+            {data?.searchRestaurant.restaurants?.map((restaurant) => (
+              <Restaurant
+                key={restaurant.id + ""}
+                id={restaurant.id + ""}
+                coverImg={restaurant.coverImg}
+                name={restaurant.name}
+                categoryName={restaurant.category?.name}
+              />
+            ))}
+          </div>
+          <div className="grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10">
+            {page > 1 ? (
+              <button
+                onClick={onPrevPageClick}
+                className="focus:outline-none text-2xl font-medium"
+              >
+                &larr;
+              </button>
+            ) : (
+              <div></div>
+            )}
+            <span>
+              Page {page} of {data?.searchRestaurant.totalPages}
+            </span>
+            {page !== data?.searchRestaurant.totalPages ? (
+              <button
+                onClick={onNextPageClick}
+                className="focus:outline-none text-2xl font-medium"
+              >
+                &rarr;
+              </button>
+            ) : (
+              <div></div>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
