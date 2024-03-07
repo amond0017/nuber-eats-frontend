c76ccad89ead1d81ebade663d14d0be48236e116
kangyesong <kys0017@gmail.com>
2024-03-07 20:16:22 +0900

    💄 Restaurant part Two - container 부분 tailwind css 스타일링

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -33,5 +33,24 @@ export const Restaurant = () => {
       },
     }
   );
-  return <div>Restaurant</div>;
+  return (
+    <div>
+      <div
+        className="bg-gray-800 bg-center bg-cover py-48"
+        style={{
+          backgroundImage: `url(${data?.restaurant.restaurant?.coverImg})`,
+        }}
+      >
+        <div className="bg-white w-3/12 py-8 pl-48">
+          <h4 className="text-4xl mb-3">{data?.restaurant.restaurant?.name}</h4>
+          <h5 className="text-sm font-light mb-2">
+            {data?.restaurant.restaurant?.category?.name}
+          </h5>
+          <h6 className="text-sm font-light">
+            {data?.restaurant.restaurant?.address}
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
 };
