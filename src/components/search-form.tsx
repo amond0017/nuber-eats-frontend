2af1f1fc1ab496f14d89a3dd7d808cc5f747e4eb
kangyesong <kys0017@gmail.com>
2024-03-07 19:26:27 +0900

    📝 search-form 컴포넌트로 추출

---
+++ src/components/search-form.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useMemo } from "react";
+import { useForm } from "react-hook-form";
+import { useHistory, useLocation } from "react-router-dom";
+
+interface ISearchFormProps {
+  searchTerm: string;
+}
+
+export const SearchForm = () => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const query = useMemo(() => location.search.split("?term=")[1], [location]);
+
+  const { register, handleSubmit, getValues, setValue } =
+    useForm<Pick<ISearchFormProps, "searchTerm">>();
+
+  useEffect(() => {
+    // '/' -> 'search' 페이지로 왔을 떄.
+    const { searchTerm } = getValues();
+
+    if (query && !searchTerm) {
+      setValue("searchTerm", query);
+    }
+  }, [query]);
+
+  const onSearchSubmit = () => {
+    const { searchTerm } = getValues();
+    history.push({
+      pathname: "/search",
+      search: `?term=${searchTerm}`,
+    });
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSearchSubmit)}
+      className="bg-gray-800 w-full py-40 flex items-center justify-center"
+    >
+      <input
+        {...register("searchTerm", { required: true })}
+        type="search"
+        className="input rounded-md border-0 w-3/4 md:w-3/12"
+        placeholder="Search Restaurants..."
+      />
+    </form>
+  );
+};
