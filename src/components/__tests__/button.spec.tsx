77dacb019e3c0cc1af5d47bb645ef00588b2733c
kangyesong <kys0017@gmail.com>
2024-03-09 01:19:05 +0900

    🧪 Button Tests - button props 에 따른 test

---
+++ src/components/__tests__/button.spec.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Button } from "../button";
+
+describe("<Button />", () => {
+  it("should render OK with props", () => {
+    render(<Button canClick={true} loading={false} actionText={`test`} />);
+
+    screen.getByText("test");
+  });
+  it("should display loading", () => {
+    const { debug, container } = render(
+      <Button canClick={false} loading={true} actionText={`test`} />
+    );
+
+    // debug();
+    screen.getByText("Loading...");
+
+    // jest
+    expect(container.firstChild).toHaveClass("pointer-events-none");
+  });
+});
