89c696d8ea3826d38f987a85e864e6da13d16f4d
kangyesong <kys0017@gmail.com>
2024-03-13 00:48:33 +0900

    📦 Our First Cypress Test - cypress 설정,  @testing-library/cypress 설치 및 설정

--- cypress/e2e/first-test.cy.ts
+++ cypress/e2e/first-test.cy.ts
@@ -1,7 +1,24 @@
-describe("First Test", () => {
-  it("should go to homapage", () => {
-    cy.visit("http://localhost:3000")
-      .title()
-      .should("eq", "Loginnnnnn | Nuber Eats");
+describe("Log In", () => {
+  it("should see login page", () => {
+    cy.visit("/").title().should("eq", "Login | Nuber Eats");
+  });
+
+  it("can fill out the form", () => {
+    cy.visit("/")
+      .findByPlaceholderText(/email/i)
+      .type("nico@nomadcoders.co")
+      .findByPlaceholderText(/password/i)
+      .type("12345")
+      .findByRole("button")
+      .should("not.have.class", "pointer-events-none");
+    // todo (can log in )
+  });
+
+  it("can see email / password validation errors", () => {
+    cy.visit("/")
+      .findByPlaceholderText(/email/i)
+      .type("bad@email")
+      .findByRole("alert")
+      .should("have.text", "Please enter a valid email");
   });
 });
