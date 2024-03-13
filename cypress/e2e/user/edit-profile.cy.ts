681db9604258f1d5b2561879eacefee9b870df1f
kangyesong <kys0017@gmail.com>
2024-03-14 01:54:03 +0900

    🧪 EditProfile E2E part Two - intercepting req. and  sending res. with fixture

--- cypress/e2e/user/edit-profile.cy.ts
+++ cypress/e2e/user/edit-profile.cy.ts
@@ -11,6 +11,13 @@ describe("Edit Profile", () => {
   });
 
   it("can change email", () => {
+    user.intercept("POST", "http://localhost:4000/graphql", (req) => {
+      console.log(req.body);
+      if (req.body?.operationName === "editProfile") {
+        // @ts-ignore
+        req.body?.variables?.input?.email = "user@user.com";
+      }
+    });
     user.visit("/edit-profile");
     user.findByPlaceholderText(/email/i).clear().type("new@nomadcoders.co");
     user.findByRole("button").click();
