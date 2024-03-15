953fb9a500bdeccb8ebf8ab650048b5796279b23
kangyesong <kys0017@gmail.com>
2024-03-15 19:54:41 +0900

    🎨 Create Restaurant part Two - 파일 input 추가, onSubmit 작성

--- src/pages/owner/add-restaurant.tsx
+++ src/pages/owner/add-restaurant.tsx
@@ -3,9 +3,11 @@ import {
   createRestaurant,
   createRestaurantVariables,
 } from "@generated/createRestaurant";
+import { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { Button } from "src/components/button";
+import { FormError } from "src/components/form-error";
 
 const CREATE_RESTAURANT_MUTATION = gql`
   mutation createRestaurant($input: CreateRestaurantInput!) {
@@ -20,13 +22,24 @@ interface IFormProps {
   name: string;
   address: string;
   categoryName: string;
+  file: FileList;
 }
 
 export const AddRestaurant = () => {
-  const [createRestaurantMutation, { loading, data }] = useMutation<
+  const onCompleted = (data: createRestaurant) => {
+    const {
+      createRestaurant: { ok, error },
+    } = data;
+    if (ok) {
+      setUploading(false);
+    }
+  };
+  const [createRestaurantMutation, { data }] = useMutation<
     createRestaurant,
     createRestaurantVariables
-  >(CREATE_RESTAURANT_MUTATION);
+  >(CREATE_RESTAURANT_MUTATION, {
+    onCompleted,
+  });
 
   const {
     register,
@@ -37,8 +50,34 @@ export const AddRestaurant = () => {
     mode: "onChange",
   });
 
-  const onSubmit = () => {
-    console.log(getValues());
+  const [uploading, setUploading] = useState(false);
+
+  const onSubmit = async () => {
+    try {
+      setUploading(true);
+      const { file, name, categoryName, address } = getValues();
+      const actualFile = file[0];
+      const formBody = new FormData();
+      formBody.append("file", actualFile);
+      const { url: coverImg } = await (
+        await fetch("http://localhost:4000/uploads", {
+          method: "POST",
+          body: formBody,
+        })
+      ).json();
+      setUploading(false);
+
+      createRestaurantMutation({
+        variables: {
+          input: {
+            name,
+            categoryName,
+            address,
+            coverImg,
+          },
+        },
+      });
+    } catch (error) {}
   };
 
   return (
@@ -46,35 +85,53 @@ export const AddRestaurant = () => {
       <Helmet>
         <title>Add Restaurant | Nuber Eats</title>
       </Helmet>
-      <h1>Add Restaurant</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          {...register("name", { required: "Name is required." })}
-          className="input"
-          type="text"
-          placeholder="Name"
-        />
-        <input
-          {...register("address", { required: "Address is required." })}
-          className="input"
-          type="text"
-          placeholder="Address"
-        />
-        <input
-          {...register("categoryName", {
-            required: "Category Name is required.",
-          })}
-          className="input"
-          type="text"
-          placeholder="Category Name"
-        />
+      <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
+        <h4 className="w-full font-medium text-center text-3xl mb-5">
+          Add Restaurant
+        </h4>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="grid gap-3 mt-5 w-full mb-5"
+        >
+          <input
+            {...register("name", { required: "Name is required." })}
+            className="input"
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            {...register("address", { required: "Address is required." })}
+            className="input"
+            type="text"
+            placeholder="Address"
+          />
+          <input
+            {...register("categoryName", {
+              required: "Category Name is required.",
+            })}
+            className="input"
+            type="text"
+            placeholder="Category Name"
+          />
 
-        <Button
-          loading={loading}
-          canClick={isValid}
-          actionText="Create Restaurant"
-        />
-      </form>
+          <div>
+            <input
+              {...register("file", { required: true })}
+              type="file"
+              accept="image/*"
+            />
+          </div>
+
+          <Button
+            loading={uploading}
+            canClick={isValid}
+            actionText="Create Restaurant"
+          />
+          {data?.createRestaurant?.error && (
+            <FormError errorMessage={data.createRestaurant.error} />
+          )}
+        </form>
+      </div>
     </div>
   );
 };
