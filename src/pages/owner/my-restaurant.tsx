0f8f1dd562b9ba82527a60d88a402b54661f4a47
kangyesong <kys0017@gmail.com>
2024-03-18 17:50:14 +0900

    📦 Victory Charts part One - victory 차트 패키지 추가

--- src/pages/owner/my-restaurant.tsx
+++ src/pages/owner/my-restaurant.tsx
@@ -2,6 +2,8 @@ import { gql, useQuery } from "@apollo/client";
 import { myRestaurant, myRestaurantVariables } from "@generated/myRestaurant";
 import React from "react";
 import { Link, useParams } from "react-router-dom";
+import { VictoryAxis, VictoryBar, VictoryChart } from "victory";
+
 import { Dish } from "src/components/dish";
 import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from "src/fragments";
 
@@ -76,6 +78,27 @@ export const MyRestaurant = () => {
               ))}
             </div>
           )}
+        </div>
+        <div className="mt-20 mb-10">
+          <h4 className="text-center text-2xl font-medium">Sales</h4>
+          <div className="max-w-lg w-full mx-auto">
+            <VictoryChart domainPadding={20}>
+              <VictoryAxis
+                label="Amount of Money"
+                dependentAxis
+                tickValues={[20, 30, 40, 50, 60]}
+              />
+              <VictoryAxis label="Days of Life" />
+              <VictoryBar
+                data={[
+                  { x: 10, y: 20 },
+                  { x: 20, y: 50 },
+                  { x: 35, y: 60 },
+                  { x: 45, y: 99 },
+                ]}
+              />
+            </VictoryChart>
+          </div>
         </div>
       </div>
     </div>
