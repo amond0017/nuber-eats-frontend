b04dd99770178e1057316ad2268f1a136943531c
kangyesong <kys0017@gmail.com>
2024-03-18 16:52:17 +0900

    🔊 apollo client error - 콘솔 표시

--- src/apollo.ts
+++ src/apollo.ts
@@ -3,13 +3,25 @@ import {
   InMemoryCache,
   createHttpLink,
   makeVar,
+  from,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import { LOCALSTORAGE_TOKEN } from "./constants";
+import { onError } from "@apollo/client/link/error";
 
 const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
 export const isLoggedInVar = makeVar(Boolean(token));
 export const authTokenVar = makeVar(token);
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path, extensions }) => {
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      );
+    });
+  if (networkError) console.error(`[Network error]: ${networkError}`);
+});
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
@@ -25,7 +37,7 @@ const authLink = setContext((_, { headers }) => {
 });
 
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: from([errorLink, authLink.concat(httpLink)]),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
