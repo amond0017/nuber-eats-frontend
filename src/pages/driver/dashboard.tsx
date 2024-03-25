293d1416e5fbade06e73090ad5d948ee0e77fdb5
kangyesong <kys0017@gmail.com>
2024-03-25 15:44:07 +0900

    ✨  Painting Directions - 경로 표시 동작

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -8,8 +8,8 @@ interface ICoords {
 
 const defaultProps = {
   center: {
-    lat: 35.22,
-    lng: 128.87,
+    lat: 35.22766130662256,
+    lng: 128.88035452609756,
   },
   zoom: 16,
 };
@@ -39,16 +39,16 @@ export const Dashboard = () => {
 
   useEffect(() => {
     if (map && maps) {
-      map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
-      const geocoder = new google.maps.Geocoder();
-      geocoder.geocode(
-        {
-          location: new google.maps.LatLng(driverCoords.lat, driverCoords.lng),
-        },
-        (result, status) => {
-          console.log(status, result);
-        }
-      );
+      //map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
+      // const geocoder = new google.maps.Geocoder();
+      // geocoder.geocode(
+      //   {
+      //     location: new google.maps.LatLng(driverCoords.lat, driverCoords.lng),
+      //   },
+      //   (result, status) => {
+      //     console.log(status, result);
+      //   }
+      // );
     }
   }, [map, maps, driverCoords.lat, driverCoords.lng]);
 
@@ -68,6 +68,52 @@ export const Dashboard = () => {
     setMaps(maps);
   };
 
+  const onGetRouteClick = () => {
+    if (map) {
+      const directionsService = new google.maps.DirectionsService();
+      const directionsRenderer = new google.maps.DirectionsRenderer({
+        polylineOptions: {
+          strokeColor: "#000",
+          strokeOpacity: 1,
+          strokeWeight: 5,
+        },
+      });
+
+      directionsRenderer.setMap(map);
+      directionsService.route(
+        {
+          travelMode: google.maps.TravelMode.DRIVING,
+          origin: {
+            location: new google.maps.LatLng(
+              driverCoords.lat,
+              driverCoords.lng
+            ),
+          },
+          destination: {
+            location: new google.maps.LatLng(
+              driverCoords.lat + 0.008,
+              driverCoords.lng + 0.008
+            ),
+          },
+        },
+        (result, status) => {
+          console.log(status, result);
+          directionsRenderer.setDirections(result);
+        }
+      );
+    }
+  };
+
+  const onGetCenterClick = () => {
+    if (map) {
+      console.log(
+        "getCenter === ",
+        map.getCenter()?.lat(),
+        map.getCenter()?.lng()
+      );
+    }
+  };
+
   return (
     <div>
       <div
@@ -81,14 +127,20 @@ export const Dashboard = () => {
           bootstrapURLKeys={{ key: process.env.REACT_APP_MAPS_API_KEY || "" }}
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={({ map, maps }) => onApiLoaded(map, maps)}
-        >
-          <Driver
-            id="nuber-driver"
-            lat={driverCoords.lat}
-            lng={driverCoords.lng}
-          />
-        </GoogleMapReact>
+        ></GoogleMapReact>
       </div>
+      <button
+        className="bg-blue-700 mx-3 p-2 border-2 border-black text-white"
+        onClick={onGetRouteClick}
+      >
+        Get route
+      </button>
+      <button
+        className="bg-blue-700 mx-3 p-2 border-2 border-black text-white"
+        onClick={onGetCenterClick}
+      >
+        Get center
+      </button>
     </div>
   );
 };
